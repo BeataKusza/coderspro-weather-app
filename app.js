a67e9a9429c8531ee25d3079678369636704deff
Beata.Kusza <anja37@o2.pl>
2017-04-08 16:39:02 +0200

    create resource for fetching data REST API

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use strict';
-
-var weatherApp = angular.module('weatherApp', ['ngRoute']);
-
+//http://api.openweathermap.org/data/2.5/forecast/daily?q=Gdansk&cnt=2&appid=9a5c0929cb85bfcc967a51c492de4b5b
+var weatherApp = angular.module('weatherApp', [
+  'ngRoute',
+  'ngResource'
+]);
+//CONFIG
 weatherApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when('/', {
@@ -13,11 +16,11 @@ weatherApp.config(['$routeProvider', function ($routeProvider) {
       controller: 'forecastController'
     })
 }]);
-
+// SERVICE
 weatherApp.service('cityService', function () {
   this.city = 'Gdansk';
 });
-
+ //CONTROLLERS
 weatherApp.controller('homeController', ['$scope', 'cityService',
 function ($scope, cityService) {
   $scope.city = cityService.city;
@@ -27,7 +30,16 @@ function ($scope, cityService) {
   })
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService',
-function($scope, cityService) {
-  $scope.city = cityService.city;
+weatherApp.controller('forecastController', ['$scope','$resource', 'cityService',
+  function ($scope, $resource, cityService) {
+    var weatherApi = $resource('http://api.openweathermap.org/data/2.5/forecast/daily');
+    $scope.weatherResult = weatherApi.get({
+      q: 'Gdansk',
+      cnt: 2,
+      appid: '9a5c0929cb85bfcc967a51c492de4b5b'
+    }).$promise.then(function (data) {
+      console.log(data);
+    });
+
+    $scope.city = cityService.city;
 }]);
