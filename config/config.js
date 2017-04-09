219e114d8e6abbd9e65ea85e84c4363141931c6c
Beata.Kusza <anja37@o2.pl>
2017-04-09 15:02:26 +0200

    slice code

---
+++ config/config.js
@@ -0,0 +1,16 @@
+// CONFIG
+angular.module('weatherApp').config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/home.html',
+      controller: 'homeController'
+    })
+    .when('/forecast', {
+      templateUrl: '/views/forecast.html',
+      controller: 'forecastController'
+    })
+    .when('/forecast/:days', {
+      templateUrl: '/views/forecast.html',
+      controller: 'forecastController'
+    })
+}]);
